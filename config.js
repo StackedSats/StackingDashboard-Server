ea51cb5d15c23faa6ca915a284625f8e718d74eb
rachit2501 <rac.sri25@gmail.com>
2021-01-10 20:08:52 +0530

    backup commit

---
+++ config.js
@@ -0,0 +1,11 @@
+export default {
+  mnemonic:
+    "buddy faculty banana harvest concert pigeon slide salt dress limb hybrid obey royal rude seat album enforce spell various clerk foil best faith cigar",
+  keyInfo: {
+    privateKey:
+      "342c787bc8db9ec1b67600698a185854905d0b00225f968319253a73aa41cff401",
+    address: "ST3K2B2FH1AYXD26WV6YZY4DAA82AZNK967BNB9BK",
+    btcAddress: "n2VrgRFbKvcesbqerVtJEC8p5Lr2LQKtmB",
+    index: 0,
+  },
+};
